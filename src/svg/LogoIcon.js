f0b3ec707c58dba003342bb19d5c48f726c2daac
omaralsayed <42531544+omaralsayed@users.noreply.github.com>
2020-06-23 01:55:50 -0400

    Update LogoIcon component

--- src/svg/LogoIcon.js
+++ src/svg/LogoIcon.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React from "react";
 
 const LogoIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
-    <path fill="#BEE3F8" d="M44,7L4,23l40,16l-7-16L44,7z M36,23H17l18-7l1,6V23z" />
+  <svg id="U" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 86.6 98.67">
+    <defs>
+      <linearGradient
+        id="linear-gradient"
+        x1={9.42}
+        y1={84.28}
+        x2={77.18}
+        y2={16.52}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0} stopColor="#7371b5" />
+        <stop offset={0.26} stopColor="#716fb2" />
+        <stop offset={0.45} stopColor="#6b68a9" />
+        <stop offset={0.62} stopColor="#605d99" />
+        <stop offset={0.77} stopColor="#514d83" />
+        <stop offset={0.91} stopColor="#3d3866" />
+        <stop offset={1} stopColor="#2f2951" />
+      </linearGradient>
+      <style>{".cls-1{fill:#494a77}.cls-2{fill:none}"}</style>
+    </defs>
     <path
-      fill="#3182CE"
-      d="M40.212,10.669l-5.044,11.529L34.817,23l0.351,0.802l5.044,11.529L9.385,23L40.212,10.669 M44,7L4,23 l40,16l-7-16L44,7L44,7z"
-    />
-    <path
-      fill="#3182CE"
-      d="M36,22l-1-6l-18,7l17,7l-2-5l-8-2h12V22z M27.661,21l5.771-2.244L33.806,21H27.661z"
+      d="M2.23 23.71L43.3 0l10.39 6v11.75H74l10.33 6L43.3 47.42 33 41.46l25.69-14.85-21.75.81 1.4-12.56-25.72 14.85zm43.24 27.46v47.44L86.6 74.86V62.92L55.86 80.67v-23.5L86.6 39.42v-12zM0 74.86l41.14 23.75V51.17l-10.39-6v35.5L10.39 68.92v-35.5L0 27.42z"
+      fill="url(#linear-gradient)"
+      id="U"
     />
   </svg>
 );
