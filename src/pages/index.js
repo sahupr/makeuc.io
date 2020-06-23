60de72de2d7c4484840b43caa351f98635ae09c5
omaralsayed <42531544+omaralsayed@users.noreply.github.com>
2020-06-23 01:18:20 -0400

    Update StatsBox text

--- src/pages/index.js
+++ src/pages/index.js
@@ -110,16 +110,16 @@ export default () => (
     />
     <section id="stats" className="py-20 lg:pt-32">
       <div className="container mx-auto text-center">
-        <LabelText className="text-gray-600">Our customers get results</LabelText>
+        <LabelText className="text-gray-600">MakeUC 2019 Statistics</LabelText>
         <div className="flex flex-col sm:flex-row mt-8 lg:px-24">
           <div className="w-full sm:w-1/3">
-            <StatsBox primaryText="+100%" secondaryText="Stats Information" />
+            <StatsBox primaryText="180+" secondaryText="Registered Students" />
           </div>
           <div className="w-full sm:w-1/3">
-            <StatsBox primaryText="+100%" secondaryText="Stats Information" />
+            <StatsBox primaryText="20+" secondaryText="Majors Represented" />
           </div>
           <div className="w-full sm:w-1/3">
-            <StatsBox primaryText="+100%" secondaryText="Stats Information" />
+            <StatsBox primaryText="60.3%" secondaryText="Underrepresented Groups" />
           </div>
         </div>
       </div>
