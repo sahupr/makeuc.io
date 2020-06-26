33c22ab8c0cf9a95add9370799ab4905da32daa9
Dev Agrawal <devagrawal09@gmail.com>
2020-06-25 23:51:48 -0400

    registration form

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -6,12 +6,14 @@ import Button from '../Button';
 const Header = () => (
   <header className="sticky top-0 bg-white shadow">
     <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
-      <div className="flex items-center text-2xl">
-        <div className="w-12 mr-3">
-          <LogoIcon />
+      <a className="text-black" href="/">
+        <div className="flex items-center text-2xl">
+          <div className="w-12 mr-3">
+            <LogoIcon />
+          </div>
+          MakeUC
         </div>
-        MakeUC
-      </div>
+      </a>
       <div className="flex mt-4 sm:mt-0">
         <AnchorLink className="px-4" href="#features">
           Features
@@ -27,7 +29,9 @@ const Header = () => (
         </AnchorLink>
       </div>
       <div className="hidden md:block">
-        <Button className="text-sm">Start Free Trial</Button>
+        <a href="/register">
+          <Button className="text-sm">Register</Button>
+        </a>
       </div>
     </div>
   </header>
